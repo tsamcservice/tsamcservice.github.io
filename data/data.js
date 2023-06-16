67a0245588d230e410ff52260e93b8cbd5effbc4
Albert Wang <albert_wang@tscprinters.com>
2023-06-16 17:52:37 +0800

    Site updated: 2023-06-16 17:52:36

--- data/data.js
+++ data/data.js
@@ -1,1 +1,6 @@
 const googleAppsScriptUrl = "https://script.google.com/macros/s/AKfycbwhcnX07ENwJdBP1WgVZI1b7-F8eq0RyXGecyiuWInEd0mt1P76LCOTpP19guh5hgjvQA/exec";
+
+// 這張會員卡片的 URL
+var card_img_url;
+// 需要跨 function 的參數
+var flexJson, userId, flexJsonMessage, cardJsonPromotional
