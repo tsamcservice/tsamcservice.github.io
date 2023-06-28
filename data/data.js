1018b885424d3829d193f141c38d44af7e59baef
Albert Wang <albert_wang@tscprinters.com>
2023-06-28 12:30:50 +0800

    Site updated: 2023-06-28 12:30:50

--- data/data.js
+++ data/data.js
@@ -35,10 +35,13 @@ function createFlexJsonList() {
 	 alert(typeof flexJsonList)
 	//  flexJsonList=JSON.stringify(flexJsonList)
 	 flexJsonList.replace(/A{3,}/g, mainTitle_1);
+	 flexJsonList.replace("textColor", textColor);
 	 alert(flexJsonList)
 	 alert(typeof flexJsonList)
 	 alert(JSON.parse(flexJsonList))
 	 alert(typeof JSON.parse(flexJsonList))
+	 alert(JSON.parse((JSON.parse(flexJsonList))))
+	 alert(typeof JSON.parse(JSON.parse(flexJsonList)))
 	 return JSON.parse(flexJsonList)
 }
 
