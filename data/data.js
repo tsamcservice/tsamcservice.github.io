1f5ef7013c2fad1ca7d4225a813627fa59ce6be4
Albert Wang <albert_wang@tscprinters.com>
2023-06-28 14:12:15 +0800

    Site updated: 2023-06-28 14:12:15

--- data/data.js
+++ data/data.js
@@ -31,10 +31,7 @@ function writeElementDefaultValues() {
 
 // flexJson_main 的框 (list可再加入cardJsonPromotional)
 function createFlexJsonList() {
-	alert(1)
-	alert(flexJsonList)
-	flexJsonList = flexJsonList.replace("box", mainTitle_1);
-	alert(flexJsonList)
+	alert(2)
 	flexJsonList = flexJsonList.replace(/A{3,}/g, mainTitle_1);
 	flexJsonList = flexJsonList.replace(/textColor/g, textColor);
 	flexJsonList = flexJsonList.replace(/mainTitle_2/g, mainTitle_2);
